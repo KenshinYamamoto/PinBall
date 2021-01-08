26b76846cccbd76abcafe9e0a998174ee4a267fd
kenshin-yamamoto <75126463+kenshin-yamamoto@users.noreply.github.com>
2021-01-08 12:37:49 +0900

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+
+    //点数を表示するテキスト
+    private GameObject pointsText;
+
+    //点数を加算する変数
+    private int points = 0;
+
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        this.pointsText = GameObject.Find("PointsText");
+    }
+
+    void Update()
+    {
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+        this.pointsText.GetComponent<Text>().text = points.ToString();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "SmallStarTag")
+        {
+            //小さい星に当たったら10点加算する
+            this.points += 10;
+        }
+        else if(other.gameObject.tag == "LargeStarTag")
+        {
+            //大きい星に当たったら20点加算する
+            this.points += 20;
+        }
+        else if(other.gameObject.tag == "SmallCloudTag")
+        {
+            //小さい雲に当たったら50点加算する
+            this.points += 50;
+        }
+        else if(other.gameObject.tag == "LargeCloudTag")
+        {
+            //大きい雲に当たったら100点加算する
+            this.points += 100;
+        }
+    }
+}
---
+++ Assets/Scripts/FripperController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+
+    //弾いたときの傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネントを取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印を押したとき左フリッパーを動かす(発展課題)
+        if (Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //右矢印を押したとき右フリッパーを動かす(発展課題)
+        if(Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //Sキーまたは下矢印キーを押した時は同時に左右のフリッパーが動くようにしましょう(発展課題)
+        if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(tag == "LeftFripperTag" || tag == "RightFripperTag")
+            {
+                SetAngle(this.flickAngle);
+            }
+        }
+
+        //矢印キーが離されたときフリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            if (tag == "LeftFripperTag" || tag == "RightFripperTag")
+            {
+                SetAngle(this.defaultAngle);
+            }
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
